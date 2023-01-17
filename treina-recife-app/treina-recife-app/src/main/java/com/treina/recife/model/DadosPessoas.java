2f9fe3b48235d64cfdc865026eedcec5e4ced66e
João Vitor Brandão <105953676+JoaoBrandaoS@users.noreply.github.com>
2023-01-17 17:36:50 -0300

    Add files via upload

---
+++ treina-recife-app/treina-recife-app/src/main/java/com/treina/recife/model/DadosPessoas.java
@@ -0,0 +1,24 @@
+package com.treina.recife.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class DadosPessoas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String cpf;
+    private String rg;
+    private String tituloEleitor;
+    private String pis;
+
+    @OneToOne
+    private Contato contato;
+
+}
