807f94e184a853dbc42def9cd76bb91cdda63571
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-26 16:40:00 -0300

    Delete Backend-prototipo/consensoApp directory

--- Backend-prototipo/consensoApp/src/main/java/com/consenso_backend/service/ServicoService.java
+++
@@ -1,9 +0,0 @@
-package com.consenso_backend.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.consenso_backend.model.Servico;
-
-public interface ServicoService extends JpaRepository<Servico,Integer>{
-    
-}
