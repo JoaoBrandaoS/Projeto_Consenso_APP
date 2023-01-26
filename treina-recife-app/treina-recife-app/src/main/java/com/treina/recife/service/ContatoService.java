2a23b2068ee2ef7bfb6c8441bd701342b7ba27dc
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-26 16:40:38 -0300

    Delete treina-recife-app/treina-recife-app directory

--- treina-recife-app/treina-recife-app/src/main/java/com/treina/recife/service/ContatoService.java
+++
@@ -1,9 +0,0 @@
-package com.treina.recife.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.treina.recife.model.Contato;
-
-public interface ContatoService extends JpaRepository<Contato, Long> {
-    
-}
--- treina-recife-app/treina-recife-app/src/main/java/com/treina/recife/service/RedesSociaisService.java
+++
@@ -1,9 +0,0 @@
-package com.treina.recife.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.treina.recife.model.RedesSociais;
-
-public interface RedesSociaisService extends JpaRepository<RedesSociais, Long> {
-    
-}
