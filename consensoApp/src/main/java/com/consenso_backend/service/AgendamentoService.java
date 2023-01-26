3053d4507558704510270050c7fc6dc8d2561dc4
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-26 16:40:24 -0300

    Delete consensoApp directory

--- consensoApp/src/main/java/com/consenso_backend/service/AgendamentoService.java
+++
@@ -1,9 +0,0 @@
-package com.consenso_backend.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.consenso_backend.model.Agendamento;
-
-public interface AgendamentoService extends JpaRepository<Agendamento, Integer> {
-    
-}
