ab969ef2861b383e03ebfccfa09b9d3584ef9024
João Vitor Brandão <joaovitorbrandao88@gmail.com>
2023-01-28 14:27:26 -0300

    Update WebConfig.java

--- selecao/consensoApp/src/main/java/com/consenso_backend/config/WebConfig.java
+++ selecao/consensoApp/src/main/java/com/consenso_backend/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**");
-        registry.addMapping("//127.0.0.1:5500");
+        
+    
     }
     
 }
